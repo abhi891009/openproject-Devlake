120c39fe17bb333e2e960528339bd1749ee23d9a
abhi891009 <33829011+abhi891009@users.noreply.github.com>
2025-05-14 16:13:07 +0000

    Commiting the CI/CD pipeline

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "Abhi100_bucket121"
+    bucket         = "abhi100bucket121"
     key            = "Terraform_state_file/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
